712937acaa6ad3ae714d36b788729d63f3adca38
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2018-09-08 02:56:54 -0300

    finishing client, updated tests

--- src/client.py
+++ src/client.py
@@ -1,71 +1,122 @@
 import numpy as np
+import threading
+import hashlib
 import struct
 import socket
+import time
 import sys
 
-def main():
-    if len(sys.argv) < 6:
-        logExit('Incorrect number of parameters!')
 
-    # parameters passed by arguments
-    filename = sys.argv[1]
-    ipPort = tuple(sys.argv[2].split(':'))
-    Wtx = int(sys.argv[3])
-    Tout = int(sys.argv[4])
-    Perror = float(sys.argv[5])
-    IP = ipPort[0]
+# takes input in argv
+filename = sys.argv[1]
+ipPort = tuple(sys.argv[2].split(':'))
+Wtx = int(sys.argv[3])
+Tout = int(sys.argv[4])
+Perror = float(sys.argv[5])
+
+
+def main():
+    # tests number of arguments
+    if len(sys.argv) < 6:
+        logExit('Incorrect number of parameters')
+
+    host = ipPort[0]
+    port = int(ipPort[1])
+
+    # extracts logs from file and get the timestamp in seconds and nanoseconds
+    with open(filename, 'r') as fp:
+        log = fp.read().splitlines()
 
     try:
-        # takes port's parameter and transforms to network byte code
-        port = socket.htons(np.uint32(ipPort[1]))
+        # creates socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # converts string to address
-        socket.inet_pton(socket.AF_INET, IP)
+        # tries to stablish connection to the server
+        sock.connect((host, port))
 
-        # creates Socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Receive no more than 1024 bytes
+        msg = sock.recv(1024)
+        print (msg.decode('ascii'))
 
-        # sets a timeout
-        s.settimeout(60)
+    except socket.error as error:
+        logExit(sock, error)
 
-        # connects to a socket on a server's side
-        # s.connect(IP)
+    slidingWindow(log, sock)
 
-    except socket.error as error_msg:
-        logExit(error_msg[1] + ', código ' + error_msg[0])
+    sock.close()
 
-    file = open(filename, 'r')
-    log = file.read().splitlines()
 
-    # counts messages in sequence
-    counter = 0
+def slidingWindow(log, sock):
+    noLog = 0
+    ws = 0
+    we = Wtx - 1
+    acks = [None] * len(log)
+    timeout = [None] * len(log)
 
     for msg in log:
+        if we > len(log) - 1:
+            break
 
-        try:
-            # sends sequence number of the log
-            nSequence = struct.pack('Q', counter)
-            s.send(nSequence)
+        if noLog >= ws and noLog <= we:
+            data = buildLog(noLog, msg)
+            sendLog(data, sock)
+            noLog += 1
+            timeout[noLog] = threading.Timer(Tout, sendLog, args=[data, sock])
 
-            # sends timestamp
+        while acks[ws] != 1:
+            pass
 
-
-            # sends log's size
-
-
-            # sends the log
-
-
-            # sends error verifier code
+        if acks[ws] == 1:
+            ws += 1
+            we += 1
 
 
-        except socket.error as error_msg:
-            logExit(error_msg[1] + ', código ' + error_msg[0])
+def sendLog(log, sock):
+    try:
+        sock.send(log)
+    except socket.error as error:
+        logExit(sock, error)
 
-        counter += 1
 
-def logExit(string):
-    sys.exit(string)
+def receiveConfirmation(sock, acks, no):
+    try:
+        data = sock.recv(36)
+    except socket.error as error:
+        logExit(sock, error)
+
+def buildLog(no, msg):
+    (sec, nanosec) = getTimestamp()
+    sz = len(msg)
+    hash = getMd5(no, sec, nanosec, sz, msg)
+
+    noLog = np.uint64(socket.htonl(no))
+    seconds = np.uint64(socket.htonl(sec))
+    nanoseconds = np.uint32(socket.htonl(nanosec))
+    size = np.uint16(socket.htonl(sz))
+    logMsg = bytes(msg, 'utf-8')
+    md5 = bytes(hash, 'utf-8')
+
+    data = struct.pack('QQLH%ds%ds' % (len(msg), len(md5),), noLog, seconds, nanoseconds, size, logMsg, md5)
+
+    return data
+
+
+def getTimestamp():
+    seconds = int(time.time())
+    ts = time.time()
+    nanoseconds = int((ts - seconds)*1000000000)
+    return (seconds, nanoseconds)
+
+
+def getMd5(noLog, seconds, nanoseconds, sizeMsg, msg):
+    data = bytes(str(noLog) + str(seconds) + str(nanoseconds) + str(sizeMsg) + msg, 'utf-8')
+    hash = hashlib.md5(data).hexdigest()
+    return hash
+
+
+def logExit(sock, msg):
+    sock.close()
+    sys.exit(msg)
 
 
 if __name__ == '__main__':
