bc5f92008e27cf5ff4f719563d2652ff0930ca4d
Gabriel Pires <gabrielpires@dcc.ufmg.br>
2018-09-23 12:40:40 -0300

    Server 100% funcional (sem a janela e  sem escrever no arquivo)

--- src/server.py
+++ src/server.py
@@ -50,11 +50,11 @@ clients = []
 
 # Função de thread
 def threaded_client(s, data, addr):
-    print("Thread created:", threading.currentThread())
+    if print_stuff is 1: print("Thread created:", threading.currentThread())
 
-    print('Incoming datagram from: ', addr[0], ':', str(addr[1]))
-    print('Received %s bytes' % len(data))
-    print("Data received: ", data)
+    if print_stuff is 1: print('Incoming datagram from: ', addr[0], ':', str(addr[1]))
+    if print_stuff is 1: print('Received %s bytes' % len(data))
+    if print_stuff is 1: print("Data received: ", data)
 
     # Fazendo o unpacking da mensagem enviada
     # O  formato "!QQLH" é o especificado na spec do TP
@@ -62,33 +62,42 @@ def threaded_client(s, data, addr):
     # L = unsigned long (32bits)      - Nanosecs do timestamp
     # H = unsigned short (16 bits)    - Tamanho da msg
 
-    msg_header = struct.unpack('!QQLH', data[:22])
+    # Lê o cabeçalho da mensagem
+
+    msg_header = struct.unpack('!QQL', data[:20])
+
+    #Lê o tamanho da mensagem
+    msg_size = struct.unpack_from("!H", data[:22], 20)
 
     # Guardo o tamanho da mensagem (lido do formato H)
-
-    msg_size = int(msg_header[3])
+    msg_size = int(msg_size[0])
 
     # Leio a mensagem dando unpack de um offset igual ao tamanho do cabeçalho (22)
-
     msg = struct.unpack_from("!"+str(msg_size)+"s", data[:22+msg_size], 22)
-    print("Message: ", msg[0])
+    if print_stuff is 1: print("Message: ", msg[0])
 
+    # Lẽ o hash md5 da mensagem
     msg_hash = struct.unpack_from("!16s", data[:22+msg_size+16], 22+msg_size)
-    print("Hash:     ", msg_hash[0])
+    if print_stuff is 1: print("Hash:     ", msg_hash[0])
 
     test_hash = hashlib.md5(data[0:22+msg_size]).digest()
-    print("Test Hash:", test_hash)
+    if print_stuff is 1: print("Test Hash:", test_hash)
 
     # Testa se o hash enviado e o testado com o cabeçalho e msg são iguais
     if (msg_hash[0] == test_hash):
-        print("Test passed! Both Hashes are the same!")
+        if print_stuff is 1: print("Test passed! Both Hashes are the same!")
     else:
         # Descartar mensagem
-        print("Hashes are not the same, message DISCARDED!")
+        if print_stuff is 1: print("Hashes are not the same, message DISCARDED!")
 
+    # Envia o ack confirmando o recebimento da mensagem
+    ack = struct.pack("!QQL", *msg_header)
+    ack_hash = hashlib.md5(ack).digest()
+    ack += ack_hash
+    if print_stuff is 1: print("ACK w/ hash:", ack)
+    s.sendto(ack, addr)
 
-    # s.sendto(data, (addr,))
-    print("\n")
+    if print_stuff is 1: print("Ack sent to client", addr[1],"\n")
 
 
 # Função principal do programa
@@ -102,18 +111,15 @@ def main():
     # Criação do socketstruct sockaddr_in
     s = socket.socket(socket.AF_INET,        # INTERNET
                       socket.SOCK_DGRAM, 0)  # UDP
-    if print_stuff == 1:
-        print("Socket created!")
+    if print_stuff is 1: print("Socket created!")
 
     # Bind
     s.bind((udp_ip, int(port)))
-    if print_stuff == 1:
-        print("Socket bind done!")
+    if print_stuff is 1: print("Socket bind done!")
 
     while True:
         # Esperando receber datagrama
-        if print_stuff == 1:
-            print("Waiting for datagram...")
+        if print_stuff is 1: print("Waiting for datagram...")
 
         (data, addr) = s.recvfrom(16384)
 
